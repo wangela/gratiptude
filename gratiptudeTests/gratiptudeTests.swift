42d5141cb03e37b93cda194f3575688a7456103e
wangela <wangela@users.noreply.github.com>
2017-08-03 01:12:47 -0700

    Initial Commit

---
+++ gratiptudeTests/gratiptudeTests.swift
@@ -0,0 +1,36 @@
+//
+//  gratiptudeTests.swift
+//  gratiptudeTests
+//
+//  Created by Angela Yu on 8/3/17.
+//  Copyright © 2017 Angela Yu. All rights reserved.
+//
+
+import XCTest
+@testable import gratiptude
+
+class gratiptudeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
